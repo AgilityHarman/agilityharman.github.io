682ea2530d88cce1f61be252f8b5960a7b1bc4cc
AgilityHarman <harmanpreetsinghsachdeva@gmail.com>
2021-09-29 03:19:10 +0000

    Deploying to gh-pages from @ AgilityHarman/agilityharman.github.io@a929b2a41cd17f27d43c6953561c2daaf12a9e58 🚀

--- _next/static/qxMTTBlJM-4xfPZ00wGPe/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/b637e9a5-a2e24fbd690d770dc8c5.js","static/chunks/3-e8d9b6a5b22a14901d40.js","static/chunks/pages/index-66072bbb07107ec937ab.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
